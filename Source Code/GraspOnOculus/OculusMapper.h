d83f80fb77d9f61b05d52b982ea51deff47e7d88
frenchjam <psyphy@comcast.net>
2016-03-26 12:37:45 +0100

    Reorganize Oculus/Windows functionality
    OculusWIN32 creates windows and buffers that are used to render
    separately into each eye with mirroring on the main screen.
    
    OculusMapper handles the transformations between world coordinates and
    the bit maps for each eye.

---
+++ Source Code/GraspOnOculus/OculusMapper.h
@@ -0,0 +1,189 @@
+#define PUNT( msg ) MessageBoxA(NULL, (msg), "OculusRoomTiny", MB_ICONERROR | MB_OK); exit(-1)
+
+struct OculusMapper
+{
+    static const bool   UseDebugContext = false;
+
+	OculusDisplayOGL	*display;
+
+	TextureBuffer		*eyeRenderTexture[2];
+    DepthBuffer			*eyeDepthBuffer[2];
+    ovrGLTexture		*mirrorTexture;
+    GLuint				 mirrorFBO;
+
+	ovrHmd				HMD;
+	ovrGraphicsLuid		luid;
+	ovrHmdDesc			hmdDesc;
+	ovrEyeRenderDesc	EyeRenderDesc[2];
+    ovrPosef            EyeRenderPose[2];
+	ovrVector3f         ViewOffset[2];
+
+	double				fTiming;
+	double				sensorSampleTime;
+
+	bool	isVisible;
+
+	OculusMapper () : mirrorFBO( 0 ) , HMD( nullptr ) , display( nullptr ) , isVisible( true )
+	{
+		eyeRenderTexture[0] = eyeRenderTexture[1] = nullptr;
+		eyeDepthBuffer[0] = eyeDepthBuffer[1] = nullptr;
+		mirrorTexture = nullptr;
+	}
+	~OculusMapper ()
+	{}
+
+	ovrResult Initialize ( OculusDisplayOGL *display ) {
+
+		this->display = display;
+		this->HMD = HMD;
+
+		ovrResult result = ovr_Create( &HMD, &luid );
+		if ( !OVR_SUCCESS( result) ) return result;
+
+		hmdDesc = ovr_GetHmdDesc(HMD);
+
+		// Setup Window and Graphics
+		// Note: the mirror window can be any size, for this sample we use 1/2 the HMD resolution
+		ovrSizei windowSize = { hmdDesc.Resolution.w / 2, hmdDesc.Resolution.h / 2 };
+		if ( !display->InitDevice( windowSize.w, windowSize.h, reinterpret_cast<LUID*>(&luid)) ) return ovrError_Initialize;
+
+		// Make eye render buffers
+		for (int eye = 0; eye < 2; ++eye)
+		{
+			ovrSizei idealTextureSize = ovr_GetFovTextureSize(HMD, ovrEyeType(eye), hmdDesc.DefaultEyeFov[eye], 1);
+			eyeRenderTexture[eye] = new TextureBuffer(HMD, true, true, idealTextureSize, 1, NULL, 1);
+			eyeDepthBuffer[eye]   = new DepthBuffer(eyeRenderTexture[eye]->GetSize(), 0);
+
+			if ( !eyeRenderTexture[eye]->TextureSet ) return ovrError_MemoryAllocationFailure;
+		}
+
+		// Create mirror texture and an FBO used to copy mirror texture to back buffer
+		result = ovr_CreateMirrorTextureGL( HMD, GL_SRGB8_ALPHA8, windowSize.w, windowSize.h, reinterpret_cast<ovrTexture**>(&mirrorTexture) );
+		if ( !OVR_SUCCESS( result ) ) return ovrError_MemoryAllocationFailure;
+
+		// Configure the mirror read buffer
+		glGenFramebuffers(1, &mirrorFBO);
+		glBindFramebuffer(GL_READ_FRAMEBUFFER, mirrorFBO);
+		glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, mirrorTexture->OGL.TexId, 0);
+		glFramebufferRenderbuffer(GL_READ_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, 0);
+		glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+
+		ovrEyeRenderDesc EyeRenderDesc[2]; 
+		EyeRenderDesc[0] = ovr_GetRenderDesc(HMD, ovrEye_Left, hmdDesc.DefaultEyeFov[0]);
+		EyeRenderDesc[1] = ovr_GetRenderDesc(HMD, ovrEye_Right, hmdDesc.DefaultEyeFov[1]);
+
+		// Turn off vsync to let the compositor do its magic
+		wglSwapIntervalEXT(0);
+		return ovrSuccess;
+	}
+
+	void Release () {
+
+		if ( mirrorFBO ) glDeleteFramebuffers(1, &mirrorFBO);
+		if ( mirrorTexture ) ovr_DestroyMirrorTexture(HMD, reinterpret_cast<ovrTexture*>(mirrorTexture));
+		for (int eye = 0; eye < 2; ++eye)
+		{
+			delete eyeRenderTexture[eye];
+			delete eyeDepthBuffer[eye];
+		}
+		if ( display ) display->ReleaseDevice();
+		if (HMD) ovr_Destroy( HMD );
+	}
+
+	// Read the tracker and compute the poses for each of the eyes.
+	void PrepareViewpoints ( void ) {
+
+        // Get eye poses, feeding in correct IPD offset
+        ViewOffset[0] = EyeRenderDesc[0].HmdToEyeViewOffset;
+		ViewOffset[1] = EyeRenderDesc[1].HmdToEyeViewOffset;
+
+        double ftiming = ovr_GetPredictedDisplayTime( HMD, 0 );
+        // Keeping sensorSampleTime as close to ovr_GetTrackingState as possible - fed into the layer
+        sensorSampleTime = ovr_GetTimeInSeconds();
+
+		ovrTrackingState hmdState = ovr_GetTrackingState( HMD, ftiming, ovrTrue );
+        ovr_CalcEyePoses( hmdState.HeadPose.ThePose, ViewOffset, EyeRenderPose );
+
+	}
+
+	// Prepare for rendering to one or the other eye.
+	// Gets the transforms that will map from world space to the space of each eye.
+	// Inputs are which eye, the position and the orientation of the HMD.
+	// Outputs are the view and projection matrices.
+	void SelectEye ( int eye ) {
+
+		// Increment to use next texture, just before writing
+        eyeRenderTexture[eye]->TextureSet->CurrentIndex = (eyeRenderTexture[eye]->TextureSet->CurrentIndex + 1) % eyeRenderTexture[eye]->TextureSet->TextureCount;
+        // Switch to eye render target
+        eyeRenderTexture[eye]->SetAndClearRenderSurface( eyeDepthBuffer[eye] );
+
+	}
+
+	void GetEyeProjections ( int eye, Vector3f position, Matrix4f orientation, Matrix4f *view, Matrix4f *projection ) {
+
+		Vector3f shiftedEyePosition = position + orientation.Transform( EyeRenderPose[eye].Position );
+		Matrix4f eyeOrientation = orientation * Matrix4f( EyeRenderPose[eye].Orientation );
+        Vector3f up = eyeOrientation.Transform( Vector3f( 0, 1, 0 ) );
+        Vector3f forward = eyeOrientation.Transform( Vector3f( 0, 0, -1 ) );
+
+        *view = Matrix4f::LookAtRH( shiftedEyePosition, shiftedEyePosition + forward, up );
+        *projection = ovrMatrix4f_Projection( hmdDesc.DefaultEyeFov[eye], 0.2f, 1000.0f, ovrProjection_RightHanded );
+
+	}
+
+	void DeselectEye ( int eye ) {
+		// Avoids an error when calling SetAndClearRenderSurface during next iteration.
+        // Without this, during the next while loop iteration SetAndClearRenderSurface
+        // would bind a framebuffer with an invalid COLOR_ATTACHMENT0 because the texture ID
+        // associated with COLOR_ATTACHMENT0 had been unlocked by calling wglDXUnlockObjectsNV.
+        eyeRenderTexture[eye]->UnsetRenderSurface();
+	}
+	
+	ovrResult BlastIt () {
+		
+		// Do distortion rendering, Present and flush/sync
+
+        // Set up positional data.
+        ovrViewScaleDesc viewScaleDesc;
+        viewScaleDesc.HmdSpaceToWorldScaleInMeters = 1.0f;
+        viewScaleDesc.HmdToEyeViewOffset[0] = ViewOffset[0];
+        viewScaleDesc.HmdToEyeViewOffset[1] = ViewOffset[1];
+
+        ovrLayerEyeFov ld;
+        ld.Header.Type  = ovrLayerType_EyeFov;
+        ld.Header.Flags = ovrLayerFlag_TextureOriginAtBottomLeft;   // Because OpenGL.
+
+        for (int eye = 0; eye < 2; ++eye)
+        {
+            ld.ColorTexture[eye] = eyeRenderTexture[eye]->TextureSet;
+            ld.Viewport[eye]     = Recti(eyeRenderTexture[eye]->GetSize());
+            ld.Fov[eye]          = hmdDesc.DefaultEyeFov[eye];
+            ld.RenderPose[eye]   = EyeRenderPose[eye];
+            ld.SensorSampleTime  = sensorSampleTime;
+        }
+		
+        ovrLayerHeader *layers = &ld.Header;
+        ovrResult result = ovr_SubmitFrame( HMD, 0, &viewScaleDesc, &layers, 1 );
+        // exit the rendering loop if submit returns an error, will retry on ovrError_DisplayLost
+        if ( !OVR_SUCCESS(result) ) return result;
+
+        isVisible = (result == ovrSuccess);
+
+        // Blit mirror texture to back buffer
+        glBindFramebuffer( GL_READ_FRAMEBUFFER, mirrorFBO );
+        glBindFramebuffer( GL_DRAW_FRAMEBUFFER, 0 );
+        GLint w = mirrorTexture->OGL.Header.TextureSize.w;
+        GLint h = mirrorTexture->OGL.Header.TextureSize.h;
+        glBlitFramebuffer(0, h, w, 0,
+                          0, 0, w, h,
+                          GL_COLOR_BUFFER_BIT, GL_NEAREST);
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+
+        SwapBuffers( display->hDC );
+
+		return ovrSuccess;
+	}
+
+};
+
+static OculusMapper oculusMapper;
