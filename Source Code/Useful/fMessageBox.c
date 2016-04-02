e32944ab6ba15ce8aa5596f66b607ce584464850
frenchjam <psyphy@comcast.net>
2016-04-02 22:36:46 +0200

    fAborrtMessageOnCondition()

--- Source Code/Useful/fMessageBox.c
+++ Source Code/Useful/fMessageBox.c
@@ -28,3 +28,21 @@ int fMessageBox( int mb_type, const char *caption, const char *format, ... ) {
 	return( MessageBox( NULL, message, caption, mb_type ) );
 		
 }
+
+void fAbortMessageOnCondition( int condition, const char *caption, const char *format, ... ) {
+
+	va_list args;
+	int items;
+
+	// The character buffer is really long so that there is little chance of overrunning it.
+	char message[10240];
+	
+	va_start(args, format);
+	items = vsprintf(message, format, args);
+	va_end(args);
+
+	if ( condition ) {
+		MessageBox( NULL, message, caption, MB_OK | MB_ICONEXCLAMATION );
+		exit( -1 );
+	}
+}
--- Source Code/Useful/fMessageBox.h
+++ Source Code/Useful/fMessageBox.h
@@ -5,6 +5,7 @@ extern "C" {
 #endif
 
 int fMessageBox( int mb_type, const char *caption, const char *format, ... );
+void fAbortMessageOnCondition( int condition, const char *caption, const char *format, ... );
 
 #ifdef __cplusplus
 }
