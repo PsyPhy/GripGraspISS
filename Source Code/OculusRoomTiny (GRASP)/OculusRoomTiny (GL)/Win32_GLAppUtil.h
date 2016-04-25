7e83c96d6e50c7a35694d27cd3b9c0de5795668a
frenchjam <psyphy@comcast.net>
2016-04-26 00:40:48 +0200

    Make OVRSDKROOT paths relative.

--- Source Code/OculusRoomTiny (GRASP)/OculusRoomTiny (GL)/Win32_GLAppUtil.h
+++ Source Code/OculusRoomTiny (GRASP)/OculusRoomTiny (GL)/Win32_GLAppUtil.h
@@ -16,7 +16,6 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  *************************************************************************************/
-
 #include "GL/CAPI_GLE.h"
 #include "Extras/OVR_Math.h"
 #include "Kernel/OVR_Log.h"
