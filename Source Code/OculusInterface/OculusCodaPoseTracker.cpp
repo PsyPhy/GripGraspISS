48d388b2bb65dc5fc4177ac3cacd4b696783ef55
frenchjam <psyphy@comcast.net>
2016-04-25 09:12:27 +0200

    Comments to optoinertial code.

--- Source Code/OculusInterface/OculusCodaPoseTracker.cpp
+++ Source Code/OculusInterface/OculusCodaPoseTracker.cpp
@@ -19,7 +19,7 @@ OculusCodaPoseTracker::OculusCodaPoseTracker( OculusMapper *mapper, CodaPoseTrac
 	secondaryCoda = coda2;
 	PoseTracker::PoseTracker();
 	// This constant deterimines how fast the inertially-computed pose will be driven
-	// toward the absolute pose taken from the CODA.
+	// toward the absolute pose taken from the CODA. A large number means that drift will be corrected slowly.
 	InertialWeighting = 10.0;
 }
 bool OculusCodaPoseTracker::Initialize( void ) { 
@@ -28,7 +28,8 @@ bool OculusCodaPoseTracker::Initialize( void ) {
 
 	// Initialize the pose of the tracker.
 	// In the final version this should be computed from the CODA contribution.
-	// For now, we just start from zero.
+	// For now, we just start from zero. Even if we leave it this way, it works,
+	// because the drift correction will eventually drive the current state to the CODA pose.
 	CopyVector( currentState.position, zeroVector );
 	CopyQuaternion( currentState.orientation, nullQuaternion );
 
@@ -42,17 +43,24 @@ bool OculusCodaPoseTracker::Initialize( void ) {
 }
 bool OculusCodaPoseTracker::Update( void ) { 
 	
-	static int cycle_counter = 0;
+	// Retrieve the sensor data from the Oculus.
 	ovrTrackingState sensorState = oculusMapper->ReadTrackingState();
 	ovrPosef headPose = sensorState.HeadPose.ThePose;
+
+	// Compute how much time since the last inertial update.
 	double time = oculusMapper->sensorSampleTime;
 	double delta_time = time - currentState.time;
 
+	// Compute a finite step rotation from gyro measurements and elapsed time.
+	// I use here a full quaternion to compute the finite rotation. There are 
+	// other linearization methods that can be used, but I don't see an advantage to using them here.
 	Vector3 rate = { sensorState.RawSensorData.Gyro.x, sensorState.RawSensorData.Gyro.y, sensorState.RawSensorData.Gyro.z };
 	Vector3 delta;
 	ScaleVector( delta, rate, delta_time / 2.0 );
 	Quaternion dQ = { delta[0], delta[1], delta[2], 0.0 };
 	dQ[M] = sqrt( 1.0 - delta[X] * delta[X] - delta[Y] * delta[Y]- delta[Z] * delta[Z] );
+
+	// Make one integration step using the computed finite rotation.
 	Quaternion newQ;
 	MultiplyQuaternions( newQ, currentState.orientation, dQ );
 	CopyQuaternion( currentState.orientation, newQ );
@@ -60,6 +68,8 @@ bool OculusCodaPoseTracker::Update( void ) {
 	// Read the absolute pose from the CODA tracker and take a step towards it.
 	// The step is achieved by taking a weighted average between the current inertial state and the CODA state.
 	// More weight is given to the inertial state to make it responsive to rapid movement.
+	// The preference is to compute the orientation with data from a single coda. We call one of them the primary
+	// unit and us it if available. If not, we try the second unit.
 	TrackerPose codaPose;
 	if ( primaryCoda && primaryCoda->GetCurrentPose( &codaPose ) ) {
 		for ( int i = 0; i < 4; i++ ) currentState.orientation[i] = InertialWeighting * currentState.orientation[i] + codaPose.orientation[i];
@@ -71,11 +81,14 @@ bool OculusCodaPoseTracker::Update( void ) {
 	}
 	else fOutputDebugString( "No CODA update.\n" );
 
+	// After the above, we have a weighted sum of inertial and CODA data. This normalization effectively turns the
+	// sum into an approximate weighted average. Note that we are algebraically averaging the quaternion as a vector.
+	// This is approximately good for orientations that are close together. Formally, we should use slerp if we want
+	// a true weighted average in rotation space with constant rotation velocity, but that is not so important here
+	// and the formulas here are much simpler.
 	NormalizeQuaternion( currentState.orientation );
 	currentState.time = time;
 	currentState.visible = true;
-
-	if ( !(cycle_counter++ % 120) ) fOutputDebugString( "%s %s\n", qstr( currentState.orientation ), vstr( currentState.position ) );
 
 	return true; 
 
@@ -87,7 +100,7 @@ bool OculusCodaPoseTracker::GetCurrentPoseIntrinsic( PsyPhy::TrackerPose *pose )
 	CopyVector( pose->position, currentState.position );
 	CopyQuaternion( pose->orientation, currentState.orientation );
 	pose->time = currentState.time;
-	// We assume that the pose is obtained for this tracker.
+	// We assume that the pose is always available for this tracker.
 	pose->visible = true;
 
 	return true; 
