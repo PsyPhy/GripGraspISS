7336c5dc3dfac38858e7a421462d93b5f73f0b03
frenchjam <psyphy@comcast.net>
2016-08-30 00:28:00 +0200

    GripTools

---
+++ Source Code/PhantomTypist/TestPhantomTypist.bat
@@ -0,0 +1,4 @@
+REM Test the phantom typist to see if I can get it to type commands to a telnet terminal.
+
+Start /B ..\Debug\PhantomTypist.exe one two three
+Telnet
---
+++ Source Code/WinPhantomTypist/TestWinPhantomTypist.bat
@@ -0,0 +1,6 @@
+REM Test the phantom typist to see if I can get it to type commands to a telnet terminal.
+
+Start ..\Debug\WinPhantomTypist.exe
+c:\windows\System32\telnet.exe
+
+
