32a46b4eb3c3fe82b7d1f22988bdf769fb8f468a
frenchjam <psyphy@comcast.net>
2016-10-01 16:54:20 +0200

    GraspPackData.bat to transfer files to DEX.

--- Source Code/Grasp/GraspPackData.bat
+++ Source Code/Grasp/GraspPackData.bat
@@ -14,22 +14,66 @@ REM Note that the '=' is treated as a blank by the command processor.
 REM We therefore look for '--ouput' and then take the next parameter as the rootname.
 :FIND
 if "%1"=="" goto NOTFOUND
-echo Leading argument is %1
 if "%1"=="--output" goto FOUND
 shift
 goto FIND
 
 :NOTFOUND 
 echo Could not find filename root.
-exit -1
+goto :eof
 
 :FOUND
-set root=%2
-echo File root is %root%
+set local_directory=%2
+:shrink
+if "%local_directory:~-1%" == "\" goto :next
+set local_directory=%local_directory:~0,-1%
+goto :shrink
 
-set local_directory=%root:~0,14%
-echo Local directory is %local_directory%
-dir %local_directory%
+:next
+echo Local directory is "%local_directory%"
+
+REM
+REM Compute the archive name from the specified local directory.
+REM Per CADMOS request, it should be YYDOY.tar, but for now we us gYYMMDD.tar.
+set yy=%local_directory:~-7,2%
+set mm=%local_directory:~-5,2%
+set dd=%local_directory:~-3,2%
+
+call :DateToOrdinal %yy% %mm% %dd% year day
+echo %yy% %mm% %dd% Year: %year% Day: %day%
+
+set gROOT=g%year%%day%
+set TARFILE=%gROOT%.tar
+set ZIPFILE=%gROOT%.gzp
+set TARMD5=%gROOT%.md5
+set FILESMD5=%gROOT%Files.md5
+echo Tar filename is "%TARFILE%"
+
+REM Here we are supposed to insert all the pertinent files into the tar.
+REM tar uses '/' instead of '\' and so does not expand 'xxx\*' correctly, so we
+REM move to the directory and insert all the files using '*'. Then we move the 
+REM .tar to where we really want it to be.
+echo Packing data into tar file: %TARFILE%
+set here=%CD%
+pushd "%local_directory%"
+REM First create an MD5 checksum for each data file.
+"%here%"\Executables\md5.exe * > %FILESMD5%
+REM Now put all the files in a tar archive, including the MD5s.
+"%here%"\Executables\tar.exe --verbose --create --file=%TARFILE% * 
+REM Move it up to the Results root.
+move /Y %TARFILE% "%here%\Results"
+popd
+
+pushd Results
+echo Zipping the tar file.
+"%here%"\Executables\gzip.exe --stdout %TARFILE% > %ZIPFILE%
+echo Split into manageable chunks.
+"%here%"\Executables\split.exe --bytes=40m %ZIPFILE% %gROOT%.
+echo Create MD5 checksums for archive and chunks.
+"%here%"\Executables\md5.exe %gRoot%.* > "%here%"\%TARMD5%
+move "%here%"\%TARMD5% .
+
+echo Transfer by FTP to GRIP.
 
 REM
 REM Copy the data files to DEX.
@@ -42,25 +86,48 @@ set GRASPROOT=DATA1
 set DIRECTORY=GDLK
 
 REM
-REM Compute the archive name based on today's date.
-REM This is bad because it depends on the regional settings.
-REM Per CADMOS request, it should be YYDOY.tar
-set yy=%date:~6,4%
-set mm=%date:~3,2%
-set dd=%date:~0,2%
-set TARFILE=g%yy%%mm%%dd%.tar
-echo Tar filename is %TARFILE%
-
-REM Here we are supposed to insert all the pertinent files into the tar.
-Executables\tar.exe --verbose --create --file=%TARFILE% *.dat *.ini *.bat
-
-REM
 REM Make the directory on the host. This might fail if the directory is already there, so ignore any errors.
 REM
-Executables\WinSCP.com /command "open ftp://%LOGONID%:%PASSWORD%@%HOST%" "cd %GRASPROOT%" "mkdir %DIRECTORY%"  "exit" 
+"%here%"\Executables\WinSCP.com /command "open ftp://%LOGONID%:%PASSWORD%@%HOST%" "cd %GRASPROOT%" "mkdir %DIRECTORY%"  "exit" 
 if ERRORLEVEL == 1 GOTO IGNORE
 :IGNORE
-Executables\WinSCP.com /command "open ftp://%LOGONID%:%PASSWORD%@%HOST%" "cd %GRASPROOT%" "cd %DIRECTORY%" "mput %TARFILE%"  "exit" 
+"%here%"\Executables\WinSCP.com /command "open ftp://%LOGONID%:%PASSWORD%@%HOST%" "cd %GRASPROOT%" "cd %DIRECTORY%" "mput %gROOT%.??"  "put  %gROOT%.md5" "exit" 
+REM if ERRORLEVEL == 1 GOTO ERREXIT
 
-
+REM Normal Exit
+REM Clean up.
+del /Q %gROOT%.?? %ZIPFILE% %TARMD5% %TARFILE%
+popd
 exit 0
+
+:ERREXIT
+echo Error during file transfer to GRIP.
+exit -1
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:DateToOrdinal %yy% %mm% %dd% year day
+::
+:: By: Ritchie Lawrence, updated 2002-11-22. Version 1.1
+::
+:: Func: Returns an ISO 8601 Ordinal date from a calendar date.
+:: For NT4/2K/XP.
+:: 
+:: Args: %1 year component to be converted, 2 or 4 digits (by val)
+::       %2 month component to be converted, leading zero ok (by val)
+::       %3 day of month to be converted, leading zero ok (by val)
+::       %4 var to receive year, 2 digits (by ref)
+::       %5 var to receive day of year, 001 to 366 (by ref)
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+setlocal ENABLEEXTENSIONS
+set yy=%1&set mm=%2&set dd=%3
+if 1%yy% LSS 200 if 1%yy% LSS 170 (set yy=20%yy%) else (set yy=19%yy%)
+set /a dd=100%dd%%%100,mm=100%mm%%%100
+set /a z=14-mm,z/=12,y=yy+4800-z,m=mm+12*z-3,j=153*m+2
+set /a j=j/5+dd+y*365+y/4-y/100+y/400-32045
+set /a y=yy+4799,k=y*365+y/4-y/100+y/400-31738,o=j-k+1
+set /a yy=yy-2000
+if %o% LSS 100 set o=0%o%&if %o% LSS 10 set o=00%o%
+endlocal&set %4=%yy%&set %5=%o%&goto :EOF
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:EOF
