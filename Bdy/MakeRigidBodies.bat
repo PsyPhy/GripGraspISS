e70076746b2ffbe99f523dd57f272708a773047f
frenchjam <psyphy@comcast.net>
2016-10-26 19:18:12 +0200

    MakerRigidBodies.bat asks for descriptive info.
    A directory gets created and the description gets added to the bdy files
    as a comment.

--- Bdy/MakeRigidBodies.bat
+++ Bdy/MakeRigidBodies.bat
@@ -8,20 +8,27 @@ REM  units because this tends to give better performance. Here one can specify
 REM  which unit to use. 
 set UNIT=1
 
+set /P NAME=Directory Name: 
+set DEST="%date:~-4%-%date:~-7,2%-%date:~-10,2% %NAME%"
+mkdir %DEST%
+
+set /P Description=Description: 
 
 REM *****************************************************************************************
 REM ***                                                                                   ***
 REM ***                                     Chest.bdy                                     ***
 REM ***                                                                                   ***
 REM *****************************************************************************************
-"..\Executables\CreateRigidBodyModelFile.exe" --unit=%UNIT% 0 1 2 3 4 5 6 7 > Chest.bdy
+"..\Executables\CreateRigidBodyModelFile.exe" --unit=%UNIT% 0 1 2 3 4 5 6 7 > %DEST%\Chest.bdy
+echo ;;; %NAME% - %Description% >>  %DEST%\Chest.bdy
 
 REM *****************************************************************************************
 REM ***                                                                                   ***
 REM ***                                     Hand.bdy                                      ***
 REM ***                                                                                   ***
 REM *****************************************************************************************
-"..\Executables\CreateRigidBodyModelFile.exe" --unit=%UNIT% 8 9 10 11 12 13 14 15 > Hand.bdy
+"..\Executables\CreateRigidBodyModelFile.exe" --unit=%UNIT% 8 9 10 11 12 13 14 15 > %DEST%\Hand.bdy
+echo ;;; %NAME% - %Description% >>  %DEST%\Hand.bdy
 
 
 REM *****************************************************************************************
@@ -29,5 +36,7 @@ REM ***                                                                         
 REM ***                                      HMD.bdy                                      ***
 REM ***                                                                                   ***
 REM *****************************************************************************************
-"..\Executables\CreateRigidBodyModelFile.exe" --unit=%UNIT% 16 17 18 19 20 21 22 23 > HMD.bdy
+"..\Executables\CreateRigidBodyModelFile.exe" --unit=%UNIT% 16 17 18 19 20 21 22 23 > %DEST%\HMD.bdy
+echo ;;; %NAME% - %Description% >>  %DEST%\HMD.bdy
 
+pause
