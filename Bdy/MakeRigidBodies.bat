7cc0a5ca10cc53e582c4cb76367d50bad7520e9e
frenchjam <psyphy@comcast.net>
2017-01-05 02:51:18 +0100

    Fix to MakeRigidBodies.bat to use legacy tracker

--- Bdy/MakeRigidBodies.bat
+++ Bdy/MakeRigidBodies.bat
@@ -3,16 +3,17 @@ REM
 REM A batch file that creates the rigid body models for GRASP.
 REM
 
+REM Usually CreateRigidBodyModelFile.exe uses the CodaRTnetContinuous tracker.
+REM But you can make it use either the Coda legacy tracker (--legacy) or the
+REM daemon tracker (--daemon) instead. In the latter case, you must start up 
+REM the daemon before running this file.
+set TRACKER=--legacy
+
 REM CreateRigidModelFile.exe uses the data from only one of the two CODA cx1 
 REM  units because this tends to give better performance. Here one can specify
-REM  which unit to use. 
-set UNIT=1
-
-REM Usually CreateRigidBodyModelFile.exe uses the CodaRTnetContinuous tracker.
-REM But you can make it use the daemon tracker instead. If you do, you must
-REM start up the daemon before running this file.
-set TRACKER=--daemon
-
+REM  which unit to use. For the DEX setup, UNIT=1 is the preferred unit. When
+REM using only one CODA one must use UNIT=0.
+set UNIT=0
 
 set /P NAME=Directory Name: 
 set DEST="%date:~-4%-%date:~-7,2%-%date:~-10,2% %NAME%"
