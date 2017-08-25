799dd98016bbb277114d7d517147ba1f022f531c
frenchjam <psyphy@comcast.net>
2017-08-25 06:08:38 +0200

    Automate runtime release for training

--- Source Code/ReleaseGrasp/ReleaseGrasp.bat
+++ Source Code/ReleaseGrasp/ReleaseGrasp.bat
@@ -33,6 +33,13 @@ pushd ..\..
 %TAR% --append %VERBOSE% --file=%ARCHIVE% RunGRASP.bat
 %TAR% --append %VERBOSE% --file=%ARCHIVE% *.ini.*
 
+REM If we are creating a training version, then by default set for mirroring mode.
+if NOT %1==Training goto :NOMIRRORING
+echo Enabling mirroring.
+echo "Mirroring on." > Mirroring.flg
+%TAR% --append %VERBOSE% --file=%ARCHIVE% Mirroring.flg
+:NOMIRRORING
+
 REM Create an empty log directory to put in the release archive.
 REM We hide the current LOG directory so as not to loose what we have there.
 REM Then we create a new empty directory, with just a readme to put in the tar archive.
@@ -45,7 +52,7 @@ rmdir /S /Q LOG
 rename HIDELOG LOG
 
 REM Create an empty Results directory to put in the release archive.
-REM We hide the current Results directory so as not to loose what we have there.
+REM We hide the current Results directory so as not to lose what we have there.
 REM Then we create a new empty directory, with just a readme to put in the tar archive.
 REM Finally, we restore the original Results file.
 rename Results HIDEResults
