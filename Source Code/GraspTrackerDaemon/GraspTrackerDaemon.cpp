4c2e39c31394857ef78b474decd5b63c713ab2cd
frenchjam <psyphy@comcast.net>
2019-02-21 06:38:51 +0100

    EPM option in GraspTrackerDaemon

--- Source Code/GraspTrackerDaemon/GraspTrackerDaemon.cpp
+++ Source Code/GraspTrackerDaemon/GraspTrackerDaemon.cpp
@@ -13,10 +13,19 @@ int main(array<System::String ^> ^args)
 	Application::SetCompatibleTextRenderingDefault(false); 
 
 	// Create the main window and run it
-	Form1^ form = gcnew Form1();
+	Form1^ form = gcnew Form1( args );
 	for ( int i = 0; i < args->Length; i++ ) {
 		if ( args[i]->StartsWith( "--hide" ) ) form->autohide = true;
-		if ( args[i]->StartsWith( "--epm" ) ) form->dexHost = DEX_EPM_SERVER;
+		if ( args[i]->StartsWith( "--epm" ) ) {
+			// Look for the DEX telemetry server on the EPM LAN,
+			// not the ETD LAN.
+			form->dexServicesHost = DEX_EPM_SERVER;
+			// There is also a specific .ini file when working over the EPM LAN
+			// that tells the tracker the IP address of the CodaRTnet server.
+			// It should be the same as the dexServicesHost, but they are 
+			// two different services and so they could conceivably be different.
+			form->codaInitFile = "EpmRTnet.ini";
+		}
 	}
 	Application::Run( form );
 	return 0;
