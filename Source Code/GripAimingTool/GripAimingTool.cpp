c0129645c632cbc17cb27d642f30a21faac7a6d9
frenchjam <psyphy@comcast.net>
2019-02-21 06:57:19 +0100

    DEX services in GripAimingTool

--- Source Code/GripAimingTool/GripAimingTool.cpp
+++ Source Code/GripAimingTool/GripAimingTool.cpp
@@ -1,7 +1,7 @@
 // GripAimingTool.cpp : main project file.
 
 #include "stdafx.h"
-
+#include "../DexServices/DexServices.h"
 #include "GripAimingForm.h"
 
 using namespace GripAimingTool;
@@ -14,12 +14,34 @@ int main(array<System::String ^> ^args)
 	// Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false); 
 
+	// Establish a connection with DEX for transmitting housekeeping and marker visibility.
+	DexServicesByProxy *dex = new DexServicesByProxy();
+	// Do not log anything.
+	dex->Initialize( nullptr );
+	dex->Connect();
+	// See if the caller has specified the user, protocol, task and step numbers. We don't change those here.
+	dex->ParseCommandLineArguments( args );
+	// Initialize the sub-step to zero and send it to the ground via DEX. 
+	// We will change this as the work progresses to indicate progress on this step.
+	dex->SendSubstep( 0 );
+	dex->SnapPicture( "PREAIM" );
+
+
 	CodaRTnetDaemonTracker *tracker = new CodaRTnetDaemonTracker();
-	tracker->Initialize( "CodaRTnet.ini" );
+	tracker->Initialize( "EpmRTnet.ini" );
 
 	// Create the main window and run it
 	GripAimingForm^ form = gcnew GripAimingForm( tracker );
 	Application::Run( form );
+
+	tracker->Quit();
+
+	// Ask DEX to take a final snapshot and then disconnect.
+	dex->SnapPicture( "POSTAIM" );
+	dex->ResetTaskInfo();
+	dex->Disconnect();
+	dex->Release();
+
 
 	return 0;
 }
